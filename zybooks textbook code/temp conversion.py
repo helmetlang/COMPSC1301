b78593a43a844551667fdcd5c97e2019c1fc8b86
HondaOwner <97011035+HondaOwner@users.noreply.github.com>
2022-10-05 15:12:47 -0400

    Add files via upload

---
+++ zybooks textbook code/temp conversion.py
@@ -0,0 +1,13 @@
+def c_to_f(c):
+    f = c * 9/5 + 32
+    return f
+
+temp_c = float(input('Enter temperature in Celsius: '))
+temp_f = None
+
+# FIXME: Call conversion function
+temp_f = c_to_f(temp_c)
+
+# FIXME: Print result
+print('Fahrenheit:' , temp_f)
+ 
