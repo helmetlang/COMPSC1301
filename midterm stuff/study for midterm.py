447f0c6c38a234eeb2b9e5668efc6237b8039d69
HondaOwner <jonawjekw@gmail.com>
2022-10-07 09:51:35 -0400

    new from 10 7 22

---
+++ midterm stuff/study for midterm.py
@@ -0,0 +1,4 @@
+# Examples when what type of list should be used in a given situation:
+# dict, tuple, list etc and when they should be used in a given example
+
+# if statements are kind of tricky when given <= >=
