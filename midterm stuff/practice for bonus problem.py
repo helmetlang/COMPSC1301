70232ce0dd8a09766cb57a3eeb771633c035bb12
HondaOwner <jonawjekw@gmail.com>
2022-10-10 09:51:03 -0400

    10 10 22

--- midterm stuff/practice for bonus problem.py
+++ midterm stuff/practice for bonus problem.py
@@ -8,22 +8,24 @@
 # "   *   "
 # mentioned earlier we were going to write a for loop
 number = int(input("Enter the height that you would like for your tree to be: "))
+# FIXME: symbol = int(input"Enter a symbol for your tree: ")
 
-def draw_tree(height):
-    # draw a tree of whatever number is inserted
+def draw_tree(height, symbol='*'):
+    # draw a tree of whatever number is inserted, up to 20 in height
     if height <= 20:
-        for row in range(height):
-
-
-
-# print height - row * spaces
-# print (2*row + 1) + '*'
-
-
-
+        for rows in range(height):
+            spaces = height - rows
+            stars = (2 * rows + 1) # odd numbers
+            print(' ' * spaces, end='')
+            print(symbol * stars)
     else: 
         print("Height has to less than or equal to 20!")
 
+print(draw_tree(number))
 
-draw_tree(3)
-draw_tree(5)
+# NOTE: print(''*(height-row))
+# print height - row * spaces
+# print (2*row + 1) + '*'
+# FIXME: make sure to remove comments late
+# draw_tree(3)
+# draw_tree(5)
--- midterm stuff/study for midterm.py
+++ midterm stuff/study for midterm.py
@@ -2,3 +2,20 @@
 # dict, tuple, list etc and when they should be used in a given example
 
 # if statements are kind of tricky when given <= >=
+
+# understanding when a while or a for loop should be used
+# or just basic overall understanding of loops altogether
+
+# FIXME: you can have a while loop nested in a while loop
+
+# study chapter 5 and 6 more thoroughly
+
+
+# FIXME: Chapter 6 notes
+# a function does not need to have a return statement
+# REALLY STUDY CHAPTER 6 LOL
+# Understanding in functions what variables you would want returned 
+
+# FIXME: about the midterm
+# 60 questions + 10 point bonus programming problem
+# Furtherst in zybooks is 6.7, 6.8 will not be on midterm
