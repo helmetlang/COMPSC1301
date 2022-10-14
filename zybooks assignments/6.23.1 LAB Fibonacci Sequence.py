7c0fb5e79827ad96072e6eee4318499abb6fb0a8
HondaOwner <jonawjekw@gmail.com>
2022-10-14 14:56:49 -0400

    Create 6.23.1 LAB Fibonacci Sequence.py

---
+++ zybooks assignments/6.23.1 LAB Fibonacci Sequence.py
@@ -0,0 +1,17 @@
+# The Fibonacci sequence begins with 0 and then 1 follows. 
+# All subsequent values are the sum of the previous two, ex: 0, 1, 1, 2, 3, 5, 8, 13. 
+# Complete the fibonacci() function, which has an index n as parameter and returns the nth value in the sequence. 
+# Any negative index values should return -1
+
+# FIXME: Use a for loop and DO NOT use recursion.
+
+
+
+
+def fibonacci(n):
+    # FIXME: Type your code here. 
+
+
+if __name__ == '__main__':
+    start_num = int(input())
+    print(f'fibonacci({start_num}) is {fibonacci(start_num)}')
