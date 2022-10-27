594f99294885f68ff9aa717e6e521837692f73f8
HondaOwner <jonawjekw@gmail.com>
2022-10-27 19:13:05 -0400

    Create 8.17 LAB Filter and sort a list.py

---
+++ zybooks assignments/8.17 LAB Filter and sort a list.py
@@ -0,0 +1,12 @@
+user_input = input().split()
+
+# get rid of negative numbers from the list input
+
+# use more variables
+
+# and then sort in ascending order using the sort method
+
+user_input = user_input.sort()
+# and finally output those numbers from the list
+
+print(user_input)
