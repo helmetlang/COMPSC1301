68ac0b0f76d911c9c275ca50d7329a0bc6fc94af
HondaOwner <jonawjekw@gmail.com>
2023-02-06 14:21:56 -0500

    hhhh
    f

---
+++ powershell stuff/codewars problems/grasshopper summation.ps1
@@ -0,0 +1,14 @@
+# Write a program that finds the summation of every number from 1 to num.
+# The number will always be a positive integer greater than 0.
+
+# For example (Input -> Output):
+
+# 2 -> 3 (1 + 2)
+# 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+function Summation([int] $n) {
+  $sum = 
+  # always positive and greater than 0
+  # takes the number and in descending order makes a sum function of all of them and then
+  # does the math to add all of those together to reach a summation
+}
---
+++ python everything/class assignments/game.py
@@ -0,0 +1,11 @@
+# game.py - Jonathan
+
+answer = input("Would you like to play a game? ")
+
+while (answer[0].lower() == "y"):
+# reads the index of the first letter entered into the input,
+# and then force the answer into lower case
+    answer = input("Would you like to play again? ")
+
+print("Thank you, have a great day!")
+
